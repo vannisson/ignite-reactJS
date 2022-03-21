cee9877b57031fa1c458ee666e363bbb05eebab5
Geovane Filho <geo.vane.20002@gmail.com>
2022-03-21 15:33:05 -0300

    Module 2 - 1/2

---
+++ 02-dtmoney/src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global"
+import { Dashboard } from "./components/Dashboard"
+export function App() {
+  return (
+    <>
+      <Header />
+      <Dashboard/>
+      <GlobalStyle />
+    </>
+  );
+}
+
